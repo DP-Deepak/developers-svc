5846075d99a9a0035f888d4c96b9f5128c977464
DP-Deepak <dkp416@gmail.com>
2020-08-26 23:03:44 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+
+const app = express()
+const PORT = process.env.PORT || 9000
+app.get('/', (req,res)=> res.send('API running'))
+
+app.listen(PORT, ()=> console.log('Server started on PORT:', PORT))
